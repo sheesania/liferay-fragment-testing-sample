d7fdf84e0e8a60f56842b57e62a2a7a614dfa239
Alison Blomenberg <sheesania@users.noreply.github.com>
2022-06-10 15:11:11 -0600

    Use setup-liferay-fragment-test module

--- src/sample-collection/sample-fragment/main.test.js
+++ src/sample-collection/sample-fragment/main.test.js
@@ -1,7 +1,5 @@
 const { screen } = require("@testing-library/dom");
-const fs = require("fs");
-const path = require("path");
-const Freemarker = require("freemarker");
+const setupLiferayFragmentTest = require("setup-liferay-fragment-test");
 
 describe("test fragment", () => {
   beforeAll((done) => {
@@ -10,40 +8,7 @@ describe("test fragment", () => {
     };
     const mockNamespace = "mockNamespace";
 
-    const fragmentConfig = JSON.parse(
-      fs.readFileSync(path.join(__dirname, "fragment.json"))
-    );
-    const { cssPath, htmlPath, jsPath } = fragmentConfig;
-
-    const css = fs.readFileSync(path.join(__dirname, cssPath));
-    const style = document.createElement("style");
-    style.innerHTML = css;
-    document.head.appendChild(style);
-
-    const html = fs.readFileSync(path.join(__dirname, htmlPath));
-    const freemarker = new Freemarker({ tagSyntax: "squareBracket" });
-    freemarker.render(
-      html,
-      {
-        fragmentEntryLinkNamespace: mockNamespace,
-        configuration: configuration,
-      },
-      (err, freemarkerResult) => {
-        if (err) {
-          throw new Error(err);
-        }
-
-        const div = document.createElement("div");
-        div.innerHTML = freemarkerResult;
-        document.body.appendChild(div);
-
-        window.fragmentElement = div;
-        window.fragmentNamespace = mockNamespace;
-        window.configuration = configuration;
-        require("./" + jsPath);
-        done();
-      }
-    );
+    setupLiferayFragmentTest(configuration, mockNamespace, done);
   });
 
   it("has 4 tab panel items", () => {
